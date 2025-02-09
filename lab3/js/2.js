aa29bd290958b747b0a5e9129f85a54c36d42ca9
Zhumabek Daniyal <zhumabed@gmail.com>
2025-02-09 19:07:40 +0500

    Update 2.js

--- lab3/js/2.js
+++ lab3/js/2.js
@@ -1,1 +1,12 @@
 
+let admin, name; // можно объявить две переменные через запятую
+
+name = "Джон";
+
+admin = name;
+
+alert( admin ); // "Джон"
+
+let ourPlanetName = "Земля";
+
+let currentUserName = "Джон";
