d060629ca6d85aa08772fc9d73995445e1123086
Zhumabek Daniyal <zhumabed@gmail.com>
2025-02-09 18:25:20 +0500

    Update 1.js

--- lab3/js/1.js
+++ lab3/js/1.js
@@ -1,1 +1,16 @@
-alert("I'm JavaScript!");
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Перед скриптом...</p>
+
+  <script>
+    alert( 'Im Daniyal!' );
+  </script>
+
+  <p>...После скрипта.</p>
+
+</body>
+
+</html>
